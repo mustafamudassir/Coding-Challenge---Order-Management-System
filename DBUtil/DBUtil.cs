14c279c5867f02a90001f61001b6a5b8b0b58f5a
mustafamudassir <85429074+mustafamudassir@users.noreply.github.com>
2023-12-16 13:34:09 +0530

    Add files via upload

---
+++ DBUtil/DBUtil.cs
@@ -0,0 +1,26 @@
+﻿// DBUtil.cs
+using System;
+using System.Data.SqlClient;
+
+namespace OrderManagementSystem.util
+{
+    public static class DBUtil
+    {
+        public static SqlConnection GetDBConn()
+        {
+            try
+            {
+                
+                string connectionString = "Data Source=localhost\\MSSQLSERVER;Initial Catalog=Order_Management_System;Integrated Security=True;";
+                var connection = new SqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while establishing the database connection: {ex.Message}");
+                throw; 
+            }
+        }
+    }
+}
