9a9b2203b563c88406cd0cfc182556f6b5eeaf28
mustafamudassir <85429074+mustafamudassir@users.noreply.github.com>
2023-12-16 13:36:02 +0530

    Add files via upload

---
+++ util/DBConnUtil.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Data.SqlClient;
+
+namespace OrderManagementSystem.util
+{
+    public static class DBConnUtil
+    {
+        public static SqlConnection GetDBConn(string connectionString)
+        {
+            try
+            {
+                var connection = new SqlConnection(connectionString);
+                connection.Open();
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while opening the database connection: {ex.Message}");
+                throw; 
+            }
+        }
+    }
+}
---
+++ util/DBPropertyUtil.cs
@@ -0,0 +1,22 @@
+﻿
+using System.IO;
+
+namespace OrderManagementSystem.util
+{
+    public static class DBPropertyUtil
+    {
+        public static string GetConnectionString(string propertyFileName)
+        {
+            string propertyFilePath = Path.Combine(Directory.GetCurrentDirectory(), propertyFileName);
+
+            if (File.Exists(propertyFilePath))
+            {
+                return File.ReadAllText(propertyFilePath);
+            }
+            else
+            {
+                throw new FileNotFoundException($"Property file '{propertyFileName}' not found.");
+            }
+        }
+    }
+}
