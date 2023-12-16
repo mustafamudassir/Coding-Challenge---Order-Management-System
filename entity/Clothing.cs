7fa9651554b6f8ac0900d1c412a784c98c1bf478
mustafamudassir <85429074+mustafamudassir@users.noreply.github.com>
2023-12-16 13:34:30 +0530

    Add files via upload

---
+++ entity/Clothing.cs
@@ -0,0 +1,26 @@
+﻿using Order_Management_System.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.entity.model
+{
+    public class Clothing : Product
+    {
+        
+        public string Size { get; set; }
+        public string Color { get; set; }
+
+        
+        public Clothing() { }
+
+        public Clothing(int productId, string productName, string description, double price, int quantityInStock, string type, string size, string color)
+            : base(productId, productName, description, price, quantityInStock, type)
+        {
+            Size = size;
+            Color = color;
+        }
+    }
+}
---
+++ entity/Electronics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order_Management_System.entity
+{
+   
+    
+        public class Electronics : Product
+        {
+            
+            public string Brand { get; set; }
+            public int WarrantyPeriod { get; set; }
+
+            
+            public Electronics() { }
+
+            public Electronics(int productId, string productName, string description, double price, int quantityInStock, string type, string brand, int warrantyPeriod)
+                : base(productId, productName, description, price, quantityInStock, type)
+            {
+                Brand = brand;
+                WarrantyPeriod = warrantyPeriod;
+            }
+        }
+    }
+
+
