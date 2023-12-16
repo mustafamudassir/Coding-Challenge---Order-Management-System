2f1844d17ca17540c7aef4e6804b1588bbb5ff52
mustafamudassir <85429074+mustafamudassir@users.noreply.github.com>
2023-12-16 13:37:28 +0530

    Add files via upload

---
+++ dao/IOrderManagementRepository.cs
@@ -0,0 +1,28 @@
+﻿
+using Order_Management_System.entity;
+using OrderManagementSystem.entity.model;
+using System.Collections.Generic;
+
+namespace OrderManagementSystem.dao
+{
+    public interface IOrderManagementRepository
+    {
+       
+        void CreateOrder(User user, List<Product> products);
+
+        
+        void CancelOrder(int userId, int orderId);
+
+       
+        void CreateProduct(User adminUser, Product product);
+
+        
+        void CreateUser(User user);
+
+        
+        List<Product> GetAllProducts();
+
+        
+        List<Product> GetOrderByUser(User user);
+    }
+}
---
+++ dao/OrderProcessor.cs
@@ -0,0 +1,51 @@
+﻿
+using Order_Management_System.entity;
+using OrderManagementSystem.dao;
+using OrderManagementSystem.entity.model;
+using System;
+using System.Collections.Generic;
+
+namespace OrderManagementSystem.dao
+{
+    public class OrderProcessor : IOrderManagementRepository
+    {
+        // Implement the methods defined in the interface
+        public void CreateOrder(User user, List<Product> products)
+        {
+            // Implement the logic to create an order or user if not present
+            Console.WriteLine("Creating Order...");
+        }
+
+        public void CancelOrder(int userId, int orderId)
+        {
+            // Implement the logic to cancel an order
+            Console.WriteLine("Canceling Order...");
+        }
+
+        public void CreateProduct(User adminUser, Product product)
+        {
+            // Implement the logic to create a product
+            Console.WriteLine("Creating Product...");
+        }
+
+        public void CreateUser(User user)
+        {
+            // Implement the logic to create a user
+            Console.WriteLine("Creating User...");
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            // Implement the logic to get all products
+            Console.WriteLine("Getting All Products...");
+            return new List<Product>(); // Replace with actual logic
+        }
+
+        public List<Product> GetOrderByUser(User user)
+        {
+            // Implement the logic to get orders by a specific user
+            Console.WriteLine($"Getting Orders for User: {user.Username}");
+            return new List<Product>(); // Replace with actual logic
+        }
+    }
+}
