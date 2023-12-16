2832c9c78af7a4d69dc6e3502f626b334aecb0b6
mustafamudassir <85429074+mustafamudassir@users.noreply.github.com>
2023-12-16 13:34:43 +0530

    Add files via upload

---
+++ exception/OrderNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System;
+
+namespace OrderManagementSystem.exception
+{
+    public class OrderNotFoundException : Exception
+    {
+        public OrderNotFoundException() : base("Order not found.")
+        {
+           
+        }
+    }
+}
+
---
+++ exception/UserNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System;
+
+namespace OrderManagementSystem.exception
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException() : base("User not found.")
+        {
+           
+        }
+    }
+}
+
