3eabfd008c0cdd6a49a6627e23d26909fb8123ba
mustafamudassir <85429074+mustafamudassir@users.noreply.github.com>
2023-12-18 15:45:34 +0530

    Update IOrderManagementRepository.cs

--- dao/IOrderManagementRepository.cs
+++ dao/IOrderManagementRepository.cs
@@ -5,24 +5,95 @@ using System.Collections.Generic;
 
 namespace OrderManagementSystem.dao
 {
-    public interface IOrderManagementRepository
+    public class OrderProcessor : IOrderManagementRepository
     {
-       
-        void CreateOrder(User user, List<Product> products);
+        private List<User> users = new List<User>();
+        private List<Product> products = new List<Product>();
+        private List<Order> orders = new List<Order>();
+        private int orderIdCounter = 1;
 
+        public void CreateOrder(User user, List<Product> products)
+        {
+            
+            if (!users.Contains(user))
+            {
+                CreateUser(user);
+            }
+
+            
+            Order order = new Order
+            {
+                OrderId = orderIdCounter++,
+                User = user,
+                Products = products,
+                OrderDate = DateTime.Now
+            };
+
+            orders.Add(order);
+
+            Console.WriteLine("Order created successfully!");
+        }
+
+        public void CancelOrder(int userId, int orderId)
+        {
+            
+            User user = users.Find(u => u.UserId == userId);
+            Order order = orders.Find(o => o.OrderId == orderId && o.User == user);
+
+            if (user != null && order != null)
+            {
+                
+                orders.Remove(order);
+                Console.WriteLine("Order canceled successfully!");
+            }
+            else
+            {
+                Console.WriteLine("User or order not found. Unable to cancel the order.");
+            }
+        }
+
+        public void CreateProduct(User adminUser, Product product)
+        {
+            
+            if (adminUser != null && adminUser.Role == "Admin")
+            {
+               
+                products.Add(product);
+                Console.WriteLine("Product created successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Admin user not found. Unable to create the product.");
+            }
+        }
+
+        public void CreateUser(User user)
+        {
+            
+            user.UserId = users.Count + 1;
+            users.Add(user);
+            Console.WriteLine("User created successfully!");
+        }
+
+        public List<Product> GetAllProducts()
+        {
+           
+            return products;
+        }
+
+        public List<Product> GetOrderByUser(User user)
+        {
         
-        void CancelOrder(int userId, int orderId);
+            List<Product> userProducts = new List<Product>();
+            foreach (var order in orders)
+            {
+                if (order.User == user)
+                {
+                    userProducts.AddRange(order.Products);
+                }
+            }
 
-       
-        void CreateProduct(User adminUser, Product product);
-
-        
-        void CreateUser(User user);
-
-        
-        List<Product> GetAllProducts();
-
-        
-        List<Product> GetOrderByUser(User user);
+            return userProducts;
+        }
     }
 }
